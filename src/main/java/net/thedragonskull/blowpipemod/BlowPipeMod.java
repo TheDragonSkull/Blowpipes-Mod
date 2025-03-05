c542bc884bdbfad7f7a5b6c2dbaf8c926e10dc51
TheDragonSkull <sergibot6000@gmail.com>
2025-03-05 08:15:13 +0100

    Registered Mod Configs

--- src/main/java/net/thedragonskull/blowpipemod/BlowPipeMod.java
+++ src/main/java/net/thedragonskull/blowpipemod/BlowPipeMod.java
@@ -6,9 +6,12 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.thedragonskull.blowpipemod.config.BlowPipeModCommonConfigs;
 import net.thedragonskull.blowpipemod.enchantment.ModEnchantments;
 import net.thedragonskull.blowpipemod.entity.ModEntities;
 import net.thedragonskull.blowpipemod.item.ModItems;
@@ -36,6 +39,8 @@ public class BlowPipeMod {
         ModTriggers.register();
 
         ModMenuTypes.MENU_TYPES.register(modEventBus);
+
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BlowPipeModCommonConfigs.SPEC, "blowpipemod-common.toml");
 
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
