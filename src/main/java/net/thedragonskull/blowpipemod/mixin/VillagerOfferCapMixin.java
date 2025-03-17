68e9b1af52b248ac91f2299d17e97e61c4ad6019
TheDragonSkull <sergibot6000@gmail.com>
2025-03-17 15:58:25 +0100

    Fixed wandering trader crash

--- src/main/java/net/thedragonskull/blowpipemod/mixin/VillagerOfferCapMixin.java
+++ src/main/java/net/thedragonskull/blowpipemod/mixin/VillagerOfferCapMixin.java
@@ -3,65 +3,41 @@ package net.thedragonskull.blowpipemod.mixin;
 import com.google.common.collect.Sets;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.npc.AbstractVillager;
-import net.minecraft.world.entity.npc.Villager;
-import net.minecraft.world.entity.npc.VillagerProfession;
-import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.npc.*;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraft.world.level.Level;
 import net.thedragonskull.blowpipemod.villager.ModVillagers;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Set;
 
-@Mixin(AbstractVillager.class)
-public abstract class VillagerOfferCapMixin extends Entity {
+@Mixin(Villager.class)
+public abstract class VillagerOfferCapMixin extends AbstractVillager{
 
-    public VillagerOfferCapMixin(EntityType<?> pEntityType, Level pLevel) {
+    public VillagerOfferCapMixin(EntityType<? extends AbstractVillager> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
+    @Shadow public abstract VillagerData getVillagerData();
 
-    /**
-     * @author TheDragonSkull
-     * @reason Remove Hunter offer cap
-     */
-    @Overwrite
-    protected void addOffersFromItemListings(MerchantOffers pGivenMerchantOffers, VillagerTrades.ItemListing[] pNewTrades, int pMaxNumbers) {
-        AbstractVillager villager = (AbstractVillager) (Object) this;
-        Villager actualVillager = (Villager) villager;
-        VillagerProfession villagerProfession = actualVillager.getVillagerData().getProfession();
+    @Inject(method = "updateTrades", at = @At("HEAD"), cancellable = true)
+    private void onUpdateTrades(CallbackInfo ci) {
+        VillagerData villagerData = this.getVillagerData();
 
-        if (villagerProfession == ModVillagers.HUNTER.get()) {
+        if (villagerData.getProfession() == ModVillagers.HUNTER.get()) {
+            MerchantOffers merchantOffers = this.getOffers();
+            VillagerTrades.ItemListing[] newTrades = VillagerTrades.TRADES.get(villagerData.getProfession()).get(villagerData.getLevel());
 
-            for (VillagerTrades.ItemListing itemListing : pNewTrades) {
-                MerchantOffer merchantOffer = itemListing.getOffer(villager, this.random);
-                if (merchantOffer != null) {
-                    pGivenMerchantOffers.add(merchantOffer);
-                }
-            }
+            this.addOffersFromItemListings(merchantOffers, newTrades, 5);
 
-        } else {
-            Set<Integer> set = Sets.newHashSet();
-            if (pNewTrades.length > pMaxNumbers) {
-                while (set.size() < pMaxNumbers) {
-                    set.add(this.random.nextInt(pNewTrades.length));
-                }
-            } else {
-                for (int i = 0; i < pNewTrades.length; ++i) {
-                    set.add(i);
-                }
-            }
-
-            for (Integer integer : set) {
-                VillagerTrades.ItemListing villagertrades$itemlisting = pNewTrades[integer];
-                MerchantOffer merchantoffer = villagertrades$itemlisting.getOffer(this, this.random);
-                if (merchantoffer != null) {
-                    pGivenMerchantOffers.add(merchantoffer);
-                }
-            }
+            ci.cancel();
         }
     }
+
 }
