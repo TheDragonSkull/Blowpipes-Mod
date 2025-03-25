cd7cd34c9b07b7f2b124068ad5ca4e2eb1f997fd
TheDragonSkull <sergibot6000@gmail.com>
2025-03-25 20:11:24 +0100

    Reworked Lure Dart

--- src/main/java/net/thedragonskull/blowpipemod/entity/custom/LureDartProjectileEntity.java
+++ src/main/java/net/thedragonskull/blowpipemod/entity/custom/LureDartProjectileEntity.java
@@ -2,34 +2,24 @@ package net.thedragonskull.blowpipemod.entity.custom;
 
 
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
+import net.thedragonskull.blowpipemod.effect.ModEffects;
 import net.thedragonskull.blowpipemod.entity.ModEntities;
-import net.thedragonskull.blowpipemod.item.ModItems;
 import net.thedragonskull.blowpipemod.particle.ModParticles;
-
-import java.util.List;
 
 public class LureDartProjectileEntity extends AbstractDart{
     private int ticksSinceImpact = 0;
     private boolean isEmbedded = false;
-
-    private int hitCount = 0;
-    private int hitCooldown = 0;
-
 
     public LureDartProjectileEntity(EntityType<? extends LureDartProjectileEntity> entityType, Level level) {
         super(entityType, level);
@@ -54,26 +44,8 @@ public class LureDartProjectileEntity extends AbstractDart{
         super.onHitEntity(result);
         Entity entity = result.getEntity();
 
-        if (entity instanceof Mob mob) {
-            mob.addEffect(new MobEffectInstance(MobEffects.GLOWING, 100, 1, false, false));
-
-            //Detect nearby monsters
-            List<Monster> nearbyMonsters = this.level().getEntitiesOfClass(
-                    Monster.class,
-                    mob.getBoundingBox().inflate(15),
-                    monster -> monster != mob && monster.isAlive()
-            );
-
-            //Make the mob attack new mob target
-            for (Monster monster : nearbyMonsters) {
-                if (monster.getType() != EntityType.WARDEN && monster.getTarget() != mob) {
-                    monster.setTarget(mob);
-                }
-
-                if (monster.getType() == EntityType.WARDEN && mob.hasEffect(MobEffects.GLOWING)) {
-                    monster.setTarget(null);
-                }
-            }
+        if (entity instanceof LivingEntity livingEntity) {
+            livingEntity.addEffect(new MobEffectInstance(ModEffects.CHARMING_AURA_EFFECT.get(), 100, 0, false, false, true));
         }
     }
 
@@ -92,82 +64,50 @@ public class LureDartProjectileEntity extends AbstractDart{
             this.detectNearbyHostileMobs();
         }
 
-        if (this.hitCooldown > 0) {
-            this.hitCooldown--;
+        if (this.level().isClientSide) {
+            this.makeParticles();
         }
 
         if (this.isEmbedded) {
             ticksSinceImpact++;
 
-            //Discard dart after 10 seconds
-            if (ticksSinceImpact >= 200) {
+            //Discard dart after 5 seconds
+            if (ticksSinceImpact >= 100) {
                 this.playSound(SoundEvents.BONE_BLOCK_BREAK, 1.0F, 1.0F);
                 this.discard();
-            }
-
-            //Delay for the particles
-            if (ticksSinceImpact >= 5) {
-                this.level().addParticle(
-                        ModParticles.LURE_GLINT_PARTICLES.get(),
-                        this.getRandomX(1.5D),
-                        this.getRandomY(),
-                        this.getRandomZ(1.5D),
-                        0, 0, 0
-                );
             }
         }
     }
 
+    private void makeParticles() {
+        this.level().addParticle(
+                ModParticles.LURE_GLINT_PARTICLES.get(),
+                this.getRandomX(1.5D),
+                this.getRandomY(),
+                this.getRandomZ(1.5D),
+                0, 0, 0
+        );
+    }
 
-    //Detect nearby monsters and make em attack the dart
+    //Detect nearby mobs and make em attack the dart
     private void detectNearbyHostileMobs() {
-        double detectionRange = 5.0D;
+        double detectionRange = 15.0D;
 
         this.level().getEntities(this, this.getBoundingBox().inflate(detectionRange), entity -> {
-            return entity instanceof Monster &&
+            return entity instanceof Mob &&
                     !(entity.getType().equals(EntityType.GIANT) || entity.getType().equals(EntityType.WARDEN));
         }).forEach(entity -> {
             if (entity instanceof Mob mob) {
                 mob.getNavigation().moveTo(this.getX(), this.getY(), this.getZ(), 1.0D);
-
-                if (mob.distanceTo(this) < 1.2D) {
-                    this.hurt(this.damageSources().mobAttack(mob), 1.0F);
-                }
             }
         });
-    }
-
-    @Override
-    public boolean hurt(DamageSource pSource, float pAmount) {
-        Entity sourceEntity = pSource.getEntity();
-        if (sourceEntity instanceof Monster) {
-
-            if (this.hitCooldown > 0) {
-                return false;
-            }
-
-            this.hitCount++;
-            this.hitCooldown = 20;
-            System.out.println(hitCount);
-
-            if (this.hitCount >= 4) {
-                this.playSound(SoundEvents.BONE_BLOCK_BREAK, 1.0F, 1.0F);
-                this.discard();
-            }
-
-            return true;
-        }
-
-        return super.hurt(pSource, pAmount);
     }
 
     @Override
     protected void onInsideBlock(BlockState state) {
         super.onInsideBlock(state);
 
-        if (state.getBlock() == Blocks.WATER) {
-            this.discard();
-        } else if (state.getBlock() == Blocks.LAVA) {
+        if (state.getBlock() == Blocks.LAVA) {
             this.discard();
         }
     }
